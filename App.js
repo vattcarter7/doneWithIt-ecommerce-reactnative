db695ad490d257122328ce303da6070623f7761c
vattcarter7 <vattcarter@gmail.com>
2020-07-09 17:13:39 +0700

    removed unused modules

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  SafeAreaView,
+  Button,
+  Alert
+} from 'react-native';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style='auto' />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1}>Hello</Text>
+      <Button title='Click Me' onPress={() => console.log('Clicked')} />
+    </SafeAreaView>
   );
 };
 
@@ -15,8 +21,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
 
